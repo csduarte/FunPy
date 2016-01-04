fd9da5622e3b3e0abb7f9299f448e3e70b22d5b6
Kamal Boparai <kboparai1@gmail.com>
2016-01-03 23:23:13 -0800

    Chapter 12

---
+++ kboparai1/ex12/sd01.py
@@ -0,0 +1,7 @@
+# raw_input(...)
+#     raw_input([prompt]) -> string
+#
+#     Read a string from standard input.  The trailing newline is stripped.
+#     If the user hits EOF (Unix: Ctl-D, Windows: Ctl-Z+Return), raise EOFError.
+#     On Unix, GNU readline is used if enabled.  The prompt string, if given,
+#     is printed without a trailing newline before reading.
---
+++ kboparai1/ex12/sd04.py
@@ -0,0 +1,12 @@
+# open(...)
+#     open(name[, mode[, buffering]]) -> file object
+#
+#     Open a file using the file() type, returns a file object.  This is the
+#     preferred way to open a file.  See file.__doc__ for further information.
+#
+# file
+#
+# os
+#
+# sys
+
